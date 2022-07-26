data.py: bug fix on attention prior computation

data.py
@@ -35,7 +35,7 @@ def beta_binomial_prior_distribution(phoneme_count, mel_count,
     mel_text_probs = []
     for i in range(1, M+1):
         a, b = scaling_factor*i, scaling_factor*(M+1-i)
-        rv = betabinom(P, a, b)
+        rv = betabinom(P - 1, a, b)
         mel_i_prob = rv.pmf(x)
         mel_text_probs.append(mel_i_prob)
     return torch.tensor(np.array(mel_text_probs))